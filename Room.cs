2a767caaf0c31913533b706d4d3d420842936e66
Ray Zheng <zhurd21@gmail.com>
2014-12-09 05:49:17 -0800

    Implement the Draw method for Room Class

--- Room.cs
+++ Room.cs
@@ -12,37 +12,80 @@ namespace RandomDungeon
         public Point origin;
         public int width;
         public int height;
-        private Point diagonalEnding;
+        public Point diagonalEnding;
 
         //default constructor
         //input: None
         //return: a Room instance at (0,0) with width and height equaling 0
-        private Room() {
+        public Room()
+        {
             origin = new Point(0, 0);
             width = 0;
             height = 0;
+            diagonalEnding = new Point(0, 0);
         }
 
         //constructor with width and height given
         //input: int width, int height
         //return: a Room at (0,0) with given width and height
-        public Room(int width, int height) :this(){
+        public Room(int width, int height)
+            : this()
+        {
             this.width = width;
             this.height = height;
+            this.diagonalEnding = Point.Add(origin, new Size(width, height));
         }
 
         //constructor with origin, width and height given
         //input: Point origin, int width, int height
         //return: a Room at given Origin with given width and height
         public Room(Point origin, int width, int height)
-            : this(width, height)
+            : this()
         {
             this.origin = origin;
+            this.width = width;
+            this.height = height;
+            this.diagonalEnding = Point.Add(origin, new Size(width-1, height-1));
         }
 
+        //check if the given point is inside current room
+        //return: true if Point is inside the room, false otherwise
+        protected bool isPointInRoom(Point p) {
+            p.Offset(origin);
+            return 0 < p.X && p.X < width && p.Y > 0 && p.Y < height;
+        }
+
+        //draw current room into given blueprint
+        // A--------B
+        // |        |
+        // C--------D
+        //return void;
         public void Draw(BluePrint bluePrint)
         {
-            throw new NotImplementedException();
+            //locate the 4 anchor points
+            Point A = origin;
+            Point B = Point.Add(origin, new Size(width-1, 0));
+            Point C = Point.Add(origin, new Size(0, height-1));
+            Point D = diagonalEnding;
+            //draw line AB
+            for(int x = A.X; x < B.X+1; x++){
+                bluePrint.design[x,A.Y] = BluePrint.Dot.Horizontal;
+            }
+            //draw line CD
+            for (int x = C.X; x < D.X+1; x++)
+            {
+                bluePrint.design[x,C.Y] = BluePrint.Dot.Horizontal;
+            }
+            //draw line AC
+            for (int y = A.Y; y < C.Y+1; y++)
+            {
+                bluePrint.design[A.X, y] = BluePrint.Dot.Vertical;
+            }
+            //draw line BD
+            for (int y = B.Y; y < D.Y+1; y++)
+            {
+                bluePrint.design[B.X, y] = BluePrint.Dot.Vertical;
+            }
         }
     }
 }
