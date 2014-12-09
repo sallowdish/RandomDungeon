8c3a8ef0e92996746d3b1bce7ad5263900e1c52c
Ray Zheng <zhurd21@gmail.com>
2014-12-08 23:54:42 -0800

    Implement BluePrint Class

--- BluePrint.cs
+++ BluePrint.cs
@@ -6,7 +6,47 @@ using System.Threading.Tasks;
 
 namespace RandomDungeon
 {
-    class BluePrint
+    class BluePrint : Drawing
     {
+        private int[,] design=null;
+        private Room[] rooms=null;
+        private Path[] paths=null;
+        private int width=0;
+        private int height=0;
+        private int numRooms=0;
+
+        private const int MAX_NUM_ROOM=5;
+
+        //default constructor
+        //return a BluePrint instance with default values
+        private BluePrint()
+        {
+
+        }
+
+        //constructor with given initial values
+        //return a BluePrint instance with given width and height values
+        public BluePrint(int width, int height)
+            : this()
+        {
+            this.width = width;
+            this.height = height;
+            this.numRooms = MAX_NUM_ROOM;
+            this.design = new int[width, height];
+        }
+
+        //constructor with given initial values
+        //return a BluePrint instance with given width and height values
+        public BluePrint(int width, int height, int numRooms): this(width,height)
+        {
+            this.width = width;
+            this.height = height;
+            this.numRooms = MAX_NUM_ROOM;
+            this.design = new int[width, height];
+        }
+        public void Draw(BluePrint bluePrint)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
