11823a66987d8c779b8f30c7adb222cc1164a005
Ray Zheng <zhurd21@gmail.com>
2014-12-08 22:52:36 -0800

    Add base classes

---
+++ Drawing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomDungeon
+{
+    interface Drawing
+    {
+        void Draw(BluePrint bluePrint);
+    }
+}
---
+++ Path.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace RandomDungeon
+{
+    class Path : Drawing
+    {
+        public Point start;
+        public Point end;
+
+        public Path() {
+            start = new Point(0, 0);
+            end = new Point(0, 0);
+        }
+
+        public Path(Point start, Point end) {
+            this.start = start;
+            this.end = end;
+        }
+    
+public void Draw(BluePrint bluePrint)
+{
+ 	throw new NotImplementedException();
+}
+}
+}
