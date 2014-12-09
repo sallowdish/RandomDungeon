b4f04bd8e8023172a4bf2846eada1c27f767b0c8
Ray Zheng <zhurd21@gmail.com>
2014-12-09 05:49:33 -0800

    Implement the Draw method for Dungeon Class

--- Dungeon.cs
+++ Dungeon.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace RandomDungeon
 {
-    class Dungeon
+    class Dungeon : Drawing
     {
+        public void Draw(BluePrint bluePrint)
+        {
+            for (int x = 0; x < bluePrint.width; x++)
+            {
+                for (int y = 0; y < bluePrint.height; y++)
+                {
+                    string symbol = "";
+                    switch (bluePrint.design[x, y])
+                    {
+                        case BluePrint.Dot.Void:
+                            symbol = " ";
+                            break;
+                        case BluePrint.Dot.Vertical:
+                            symbol = "|";
+                            break;
+                        case BluePrint.Dot.Horizontal:
+                            symbol = "-";
+                            break;
+                        case BluePrint.Dot.EntryExit:
+                            symbol = "*";
+                            break;
+                        default:
+                            symbol = "x";
+                            break;
+                    }
+                    Console.Write(symbol);
+                }
+                Console.WriteLine("");
+            }
+        }
     }
 }
